50f53651a9a4d117eb5071311ae78aa0b8dc358d
Theaveas So <theaveasso@Theaveass-MacBook-Pro.local>
2022-08-23 21:31:36 +0700

    revert

--- .vscode/extensions/sleistner.vscode-fileutils-3.5.0/out/src/lib/config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getConfiguration = void 0;
-const vscode_1 = require("vscode");
-function getConfiguration(key) {
-    return vscode_1.workspace.getConfiguration("fileutils", null).get(key);
-}
-exports.getConfiguration = getConfiguration;
