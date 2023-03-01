efbd90ccd4285533cfa4f214d34305b4d94fb982
Theaveas So <theaveas.so@gmail.com>
2023-03-01 07:57:17 +0700

    update config add lsp gitsigns

---
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "bash",
+        "help",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "go",
+        "regex",
+        "tsx",
+        "typescript",
+        "vim",
+        "yaml",
+      },
+    },
+  },
+}
