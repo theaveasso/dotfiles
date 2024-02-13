4be7cc1ce0636b82804057981bda784a948cecaf
Theaveas So <theaveas.so@gmail.com>
2024-02-13 10:54:04 +0700

    update using nvim tree

---
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -0,0 +1,29 @@
+return {
+  {
+    "nvim-tree/nvim-tree.lua",
+    version = "*",
+    lazy = false,
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      local Map = require("utils").map
+
+      -- Mapping
+      Map("n", "<C-n>", "<cmd>NvimTreeToggle<CR>", { desc = "NeoTree" })
+
+      require("nvim-tree").setup({
+        sort = {
+          sorter = "case_sensitive",
+        },
+        view = {
+          width = 30,
+        },
+        renderer = {
+          group_empty = true,
+        },
+        filters = {
+          dotfiles = true,
+        },
+      })
+    end,
+  },
+}
